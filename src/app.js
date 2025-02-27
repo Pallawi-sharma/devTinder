8bb92b1b64359d6ecaaa14c60bd9921611bed689
pallawi <pallawisharma02@gmail.com>
2025-02-27 08:48:12 +0530

    added multiple route handler for a single route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ app.use("/test",(req, res) => {
   res.send("Hello from server");
 });
 
+// app.get("/user", (req,res) => {
+//   res.send("get user");
+// });
+// app.post("/user", (req,res) => {
+//   res.send("post user"); 
+// })
+
+app.get("/user", (req, res, next) => {
+  // res.send("1st route handler");
+  next();
+},
+(req, res, next) => {
+  res.send("2nd route handler")
+})
+
 app.listen(3000, () => {
   console.log("server started successfully at 3000");
 });
